d11d01f08b914a917d76f1b8b089f802caf19f5c
kacperborowieckb <kacperborowiec.kb@gmail.com>
2023-03-13 16:26:42 +0100

    finished pop

--- [lvl 1] Karta na blogu/Starter-code/src/pages/home-page/scripts/home-page.js
+++ [lvl 1] Karta na blogu/Starter-code/src/pages/home-page/scripts/home-page.js
@@ -1,8 +1,11 @@
 /// Homepage JavaScript File
 /// Here we import all the JavaScript files we need for our homepage.
 
-import '../styles/home-page.scss'
+import "../styles/home-page.scss";
+import "../styles/user.scss";
+import "../styles/text-content.scss";
+import "../styles/button.scss";
 
-import { setupCounter } from '../../../global-scripts/scripts/counter/counter.js'
+import { setupCounter } from "../../../global-scripts/scripts/counter/counter.js";
 
-setupCounter(document.querySelector('#counter'))
+setupCounter(document.querySelector("#counter"));
